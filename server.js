5f429ee115a5df811a70798ddf404aabb8c92fdb
clintfidel <clintfidel@gmail.com>
2018-01-28 00:56:39 +0100

    import route for blog

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import winston from 'winston';
 import bodyParser from 'body-parser';
+import BlogRouter from './server/routes/blogRouter';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.use('/api/v1/blog', BlogRouter);
+
 app.get('/', (req, res) => {
-    res.send('testing out Mock-data');
-  });
+  res.send('testing out Mock-data');
+});
 
 const port = process.env.PORT || 5300;
 
