93a7b9255a97f3444de8149c6c259a68d7a2fd30
clintfidel <clintfidel@gmail.com>
2018-01-28 00:58:58 +0100

    feature: implement add blog

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -1,1 +1,35 @@
-import { r } from '../models/index';
+import dummyDb from '../models/index';
+
+
+const {
+  blogModel
+} = dummyDb;
+
+const BlogController = {
+  addBlog(req, res) {
+    const myBlog = {
+      id: blogModel.length + 1,
+      blogTitle: req.body.blogTitle,
+      blogPost: req.body.blogPost,
+      views: 0
+    };
+    Object.assign(req.body, myBlog);
+    blogModel.push(req.body);
+    res.status(201).json({
+      message: 'added succesfully',
+      users: blogModel[blogModel.length - 1]
+    });
+  },
+
+  getAllBlog(req, res) {
+    const result = [];
+    for (let i = 0; i < blogModel.length; i += 1) {
+      result.push(blogModel[i]);
+    }
+    res.status(200).send(result);
+  }
+
+
+};
+
+export default BlogController;
