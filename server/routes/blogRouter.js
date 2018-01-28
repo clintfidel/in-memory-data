9ee9a7de17de796c6ca7d7cbf4571e372384e6e0
clintfidel <clintfidel@gmail.com>
2018-01-28 01:47:13 +0100

    feature: create endpoint(route) for modify-blog

--- server/routes/blogRouter.js
+++ server/routes/blogRouter.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import Blog from '../controllers/BlogController';
 
-const { addBlog, getAllBlog } = Blog;
+const { addBlog, getAllBlog, modifyBlog } = Blog;
 
 const blogRouter = express.Router();
 
 blogRouter.route('/')
   .post(addBlog)
   .get(getAllBlog);
+
+blogRouter.route('/edit/:blogId')
+  .put(modifyBlog);
 export default blogRouter;
